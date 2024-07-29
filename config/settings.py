8ceba459057555988bbe0b36268dfbb9b01fbeeb
ismgonza <ijgr11@gmail.com>
2024-07-28 22:10:07 -0600

    static files

--- config/settings.py
+++ config/settings.py
@@ -18,8 +18,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Default url behaviors
 LOGIN_URL = 'login'
