4a5e6f7ae7de90098848db1268be3c5a0c09ac33
ismgonza <ijgr11@gmail.com>
2024-07-29 07:44:02 -0600

    .

--- gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = 'gthread'
-threads = 4
-timeout = 120
-keepalive = 5
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 python manage.py migrate
-python manage.py collectstatic --noinput
 gunicorn medexp_app.wsgi:application --bind 0.0.0.0:8888
