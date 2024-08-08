c3c585a458a7cbf079197cbf60a51f19a1c237ce
ismgonza <ijgr11@gmail.com>
2024-08-07 20:34:35 -0600

    update for DO

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,10 +1,15 @@
 from config.settings.base import *
+from dotenv import load_dotenv
+from config.logging import *
+
+# Load environment variables from .env file
+load_dotenv(Path.joinpath(BASE_DIR, '.env_devel'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'admin123'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -16,8 +21,16 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME', default=''),
+        'USER': os.getenv('DB_USER', default=''),
+        'PASSWORD': os.getenv('DB_PASSWORD', default=''),
+        'HOST': os.getenv('DB_HOST', default=''),
+        'PORT': os.getenv('DB_PORT', default=''),
+        'CONN_MAX_AGE': 600,  # connection persistence for 10 minutes
+        # 'OPTIONS': {
+        #     'connect_timeout': 5,
+        # }
     }
 }
 
