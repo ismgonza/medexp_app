cde54461b1d1b919045c555b5a9f12f12b875071
ismgonza <ijgr11@gmail.com>
2024-08-08 20:25:11 -0600

    fixed permissions for reports

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    # You don't need to add any fields here if you're not storing report data
+    class Meta:
+        managed = False  # This tells Django not to create a database table for this model
+        permissions = [
+            ("view_reports", "Can view reports"),
+        ]
