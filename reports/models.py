74edb65aec9939001ef7c44f7b38b3611de71917
ismgonza <ijgr11@gmail.com>
2024-08-08 20:58:56 -0600

    Disable autogenerated permission for reports

--- reports/models.py
+++ reports/models.py
@@ -4,6 +4,7 @@ class Report(models.Model):
     # You don't need to add any fields here if you're not storing report data
     class Meta:
         managed = False  # This tells Django not to create a database table for this model
+        default_permissions = ()  # This disables the auto-generated permissions
         permissions = [
             ("view_reports", "Can view reports"),
         ]
